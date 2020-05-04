1fcec990a054ac0fd0cf5e56e220578cfc0afeef
Varkoff <virgileboris96@hotmail.fr>
2020-05-04 17:23:33 +0200

    Ajout de la compilation SCSS

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Find the Precious !</title>
+    <meta name="author" content="">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="assets/styles.css" rel="stylesheet">
+</head>
+
+<body>
+
+<p>Hello, world!</p>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+</body>
+
+</html>
